d74bc97c5c024c58826b24a87b760be40ab2f4b7
Kescovilla <kescovilla@gmail.com>
2021-11-17 12:51:07 -0800

    Water Level / ADC - Kenneth Escovilla

--- CPE_Project/CPE_Project.ino
+++ CPE_Project/CPE_Project.ino
@@ -4,6 +4,13 @@
 #include <DS3231.h>
 #include <dht_nonblocking.h>
 
+// Define ADC 
+
+volatile unsigned char* my_ADMUX = 0x7C;
+volatile unsigned char* my_ADCSRB = 0x7B;
+volatile unsigned char* my_ADCSRA = 0x7A;
+volatile unsigned int* my_ADC_DATA = 0x78;
+
 // Define Port E Register Pointers
 volatile unsigned char* port_e = (unsigned char*) 0x2E;
 volatile unsigned char* ddr_e  = (unsigned char*) 0x2D;
@@ -135,4 +142,54 @@ void write_ph(unsigned char pin_num, unsigned char state)
   {
     *port_h |= 0x01 << pin_num;
   }
+}
+
+
+void adc_init()
+{
+  // set up the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 3 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 2-0 to 0 to set prescaler selection to slow reading
+  
+  // set up the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11110111; // clear bit 2-0 to 0 to set free running mode
+  
+  // set up the MUX Register
+  *my_ADMUX  &= 0b00000000; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // reset the channel and gain bits
+  *my_ADMUX &= 0b11100000;
+  
+  // clear the channel selection bits
+  *my_ADCSRB &= 0b11110111;
+  
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    
+    // set MUX bit 
+    *my_ADCSRB |= 0b00001000;
+  }
+  
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  
+  // set bit ?? of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0b01000000;
+  
+  // wait for the conversion to complete
+  while(*my_ADCSRA & 0b000000000);
+  
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
 }
