b6b3ce3bb565d554d3ab888bba10ccbd892db23e
Watson-John <watson221@nevada.unr.edu>
2021-11-23 13:10:34 -0800

    States in loop mostly done - echo functionality still missing

--- CPE_Project/CPE_Project.ino
+++ CPE_Project/CPE_Project.ino
@@ -21,7 +21,6 @@ volatile unsigned char* pin_e  = (unsigned char*) 0x2C;
 static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
-// Water Level Sensor Input ~ Pin 3 - PE5
 // Servo                    ~ Pin 5 - PE3 (library)
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -39,7 +38,7 @@ volatile unsigned char* pin_h  = (unsigned char*) 0x100;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// ~~~~~~~~~~~ Define Port H Register Pointers ~~~~~~~~~~~~~
+// ~~~~~~~~~~~ Define Port L Register Pointers ~~~~~~~~~~~~~
 volatile unsigned char* port_L = (unsigned char*) 0x10B;
 volatile unsigned char* ddr_L  = (unsigned char*) 0x10A;
 volatile unsigned char* pin_L  = (unsigned char*) 0x109;
@@ -49,6 +48,7 @@ int Red = 5; // 44
 int Green = 4; // 45
 int Blue = 3; // 46
 
+// ~~ PWM Set Up ~~
 // Timer/ Counter 5 Register A ~  Pointer to Timer 5
 volatile unsigned char* TCCR_5C  = (unsigned char*) 0x120;
 
@@ -60,10 +60,26 @@ int WGM_5_1 = 1;
 // Output Compare Register 5 B ~ This is where we store the Duty Cycle
 volatile unsigned char* OCR_5B  = (unsigned char*) 0x12A;
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~ Analog Inputs ~~~~~~~~~~~~~~~~~~~~~
+// Water Level Sensor Input
+int water_level_pin = 0;
+
+// Servo Potentiometer
+int servo_pot_pin = 2;
+
+// ~~~~~~~~~~~~~~~~~~~ Define LCD Pins ~~~~~~~~~~~~~~~~~~~~~
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(27, 26, 25, 24, 23, 22);
+LiquidCrystal lcd(22, 23, 24, 25, 26, 27);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+// ~~~~~~~~~~~~~~~~~~~ Global Thresholds ~~~~~~~~~~~~~~~~~~~~~
+float temperature_threshold = 22.0;
+int   water_level_threshold = 80;
+enum all_states {IDlE, DISABLED, ERROR, RUNNING};
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -90,6 +106,12 @@ void setup() {
   *ddr_L |= 0x01 << Green;
   *ddr_L |= 0x01 << Blue;
 
+  // setup the LCD
+  lcd.begin(16, 2);
+
+  // setup the ADC
+  adc_init();
+
   // setup the UART
   Serial.begin(9600);
 }
@@ -97,43 +119,171 @@ void setup() {
 
 void loop() {
 
-
   float temperature;
-  float humidity;
+  unsigned int water_level;
 
+  static all_states state;
 
-  if ( getTemperature( &temperature, &humidity ) == true ){
-    Serial.print( "T = " );
-    Serial.println( temperature, 1 );
-
+  if (/*button interupt flag ON*/ ) {
+    state = IDLE;
   } else {
-    dc_fan();
+    state = DISABLED;
   }
 
+  switch (state) {
+    case IDLE:
+      print_temp_humid();
+      LED_color('g');
 
 
-  LED_color('y');
+      // Servo Active
+      pot_to_servo(1);
+
+      // Motor OFF
+      dc_fan(0);
+
+      getTemperature( &temperature, &humidity);
+      water_level = adc_read(water_level_pin);
+
+      // ~~ Exit Condition ~~
+      if ( temperature > temperature_threshold) {
+        state = RUNNING;
+        break;
+      } else if ( water_level < water_level_threshold) {
+        state = ERROR;
+        break;
+      }
+      break;
+
+    case DISABLED:
+      lcd.clear();
+      LED_color('y');
+
+      // Servo Disabled
+      pot_to_servo(0);
+
+      // Motor OFF
+      dc_fan(0);
+
+      // ~~ Exit Condition ~~
+      if ( /*button = !Kill*/) {
+        state = IDLE;
+        break;
+      }
+
+      break;
+    case RUNNING:
+      print_temp_humid();
+      LED_color('y');
+
+      // Servo Active
+      pot_to_servo(1);
+
+      //Motor ON
+      dc_fan(1);
+
+      getTemperature( &temperature, &humidity);
+      water_level = adc_read(water_level_pin);
+
+      // ~~ Exit Condition ~~
+      if ( temperature < temperature_threshold) {
+        state = IDLE;
+        break;
+      } else if ( water_level < water_level_threshold) {
+        state = ERROR;
+        break;
+      }
+
+      break;
+    case ERROR:
+      lcd.setCursor(0, 0);
+      lcd.print(" ERROR ");
+      lcd.setCursor(0, 1);
+      lcd.print("Please Check Water Level ");
+      LED_color('r');
+
+      // Servo Active
+      pot_to_servo(1);
+
+      // Motor OFF
+      dc_fan(0);
+
+
+      getTemperature( &temperature, &humidity);
+      water_level = adc_read(water_level_pin);
+
+      // ~~ Exit Condition ~~
+      if ( water_level > water_level_threshold) {
+        state = IDLE;
+        break;
+      }
+      break;
+    default:
+      lcd.setCursor(0, 0);
+      lcd.print(" ERROR ");
+      lcd.setCursor(0, 1);
+      lcd.print("Please Check Water Level ");
+      LED_color('r');
+
+      // Servo Active
+      pot_to_servo(1);
+
+      // Motor OFF
+      dc_fan(0);
+
+
+      getTemperature( &temperature, &humidity);
+      water_level = adc_read(water_level_pin);
+
+      // ~~ Exit Condition ~~
+      if ( water_level > water_level_threshold) {
+        state = IDLE;
+        break;
+      }
+      break;
+  }
+
 
 }
 
-
-// ~~ DC Fan Code ~~
-void dc_fan() {
-  //getTemp = true - in range ??
+//  ~~ Display Temp/Humidity ~~
+void print_temp_humid() {
   float temperature;
   float humidity;
 
-  if ( getTemperature( &temperature, &humidity ) == true ) {
-    //Fan off\
+  getTemperature( &temperature, &humidity);
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: ");
+  lcd.print(humidity);
+}
+
+// ~~ DC Fan Code ~~
+void dc_fan(bool state) {
+
+
+  if ( state  == false ) {
+    //Fan off
     // 5 and 4 and 3 off
     *port_h &= 0x00;
-  }
-  else {
+  } else {
     //Fan on
     //5 and 4 on 3 off
     *port_h |= 0x30;
   }
+}
 
+// ~~ Potentiometer to Servo Enable/Disable ~~
+void pot_to_servo(bool state) {
+
+  if (state == 1) {
+    unsigned int val = adc_read(potpin);
+    val = map(val, 0, 1023, 0, 180);
+    myservo.write(val);
+    delay(15);
+  }
 
 }
 
@@ -179,7 +329,7 @@ void LED_color(char color) {
   }
 
 }
-void write_pL(unsigned char pin_num, unsigned char state){
+void write_pL(unsigned char pin_num, unsigned char state) {
   if (state == 0) {
     *port_L &= ~(0x01 << pin_num);
 
@@ -189,8 +339,8 @@ void write_pL(unsigned char pin_num, unsigned char state){
   }
 }
 
-// ~~ Analog to Digital Coversion - Water Level Sensor ~~
-void adc_init(){
+// ~~ Analog to Digital Coversion - Water Level Sensor And Servo Potentiometer ~~
+void adc_init() {
   // set up the A register
   *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
   *my_ADCSRA &= 0b11011111; // clear bit 5 to 0 to disable the ADC trigger mode
@@ -207,7 +357,7 @@ void adc_init(){
   *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
   *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
 }
-unsigned int adc_read(unsigned char adc_channel_num){
+unsigned int adc_read(unsigned char adc_channel_num) {
   // reset the channel and gain bits
   *my_ADMUX &= 0b11100000;
 
