e1f8a6a4cb0b774a623d67e9176468577898000e
Watson-John <watson221@nevada.unr.edu>
2021-12-03 22:02:04 -0800

    Final Code

--- CPE_Project/CPE_Project.ino
+++ CPE_Project/CPE_Project.ino
@@ -12,44 +12,42 @@ volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~~~~~~~~~~ Define Port E Register Pointers ~~~~~~~~~~~~~
+// Temperature and Humidity Input ~ Pin 2 - PE4
 volatile unsigned char* port_e = (unsigned char*) 0x2E;
 volatile unsigned char* ddr_e  = (unsigned char*) 0x2D;
 volatile unsigned char* pin_e  = (unsigned char*) 0x2C;
 
-// Temperature and Humidity Input ~ Pin 2 - PE4
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
-// Servo                    ~ Pin 5 - PE3 (library)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 // ~~~~~~~~~~~ Define Port H Register Pointers ~~~~~~~~~~~~~
-volatile unsigned char* port_h = (unsigned char*) 0x102;
-volatile unsigned char* ddr_h  = (unsigned char*) 0x101;
-volatile unsigned char* pin_h  = (unsigned char*) 0x100;
-
-// ~~ Port H ~~~
 // Fan Motor:
 // Pin 17 - PH5 - Enable from L293D
 // Pin 16 - PH4 - In 1 OutPut
 // Pin 15 - PH3 - In 2 Output
+volatile unsigned char* port_h = (unsigned char*) 0x102;
+volatile unsigned char* ddr_h  = (unsigned char*) 0x101;
+volatile unsigned char* pin_h  = (unsigned char*) 0x100;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~~~~~~~~~~ Define Port L Register Pointers ~~~~~~~~~~~~~
+// RGB LED Control
 volatile unsigned char* port_L = (unsigned char*) 0x10B;
 volatile unsigned char* ddr_L  = (unsigned char*) 0x10A;
 volatile unsigned char* pin_L  = (unsigned char*) 0x109;
 
-// RGB LED Control
-int Red = 5; // 44
+int Yellow = 5; // 44
 int Green = 4; // 45
 int Blue = 3; // 46
+int Red = 2; // 47
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~ PWM Set Up ~~
 // Timer/ Counter 5 Register A ~  Pointer to Timer 5
-volatile unsigned char* TCCR_5C  = (unsigned char*) 0x120;
+volatile unsigned char* TCCR_5A  = (unsigned char*) 0x120;
 
 // Output Compare Pins ~ Turns on Alternatve Functionality ↓
 int COM_5_B1 = 5;
@@ -58,7 +56,6 @@ int WGM_5_1 = 1;
 
 // Output Compare Register 5 B ~ This is where we store the Duty Cycle
 volatile unsigned char* OCR_5B  = (unsigned char*) 0x12A;
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~~~~~~~~~~~~~~~~~~ Analog Inputs ~~~~~~~~~~~~~~~~~~~~~
@@ -66,20 +63,50 @@ volatile unsigned char* OCR_5B  = (unsigned char*) 0x12A;
 int water_level_pin = 0;
 
 // Servo Potentiometer
-int servo_pot_pin = 2;
+int servo_pot_pin = 8;
+Servo myservo;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // ~~~~~~~~~~~~~~~~~~~ Define LCD Pins ~~~~~~~~~~~~~~~~~~~~~
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(22, 23, 24, 25, 26, 27);
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 // ~~~~~~~~~~~~~~~~~~~ Global Thresholds ~~~~~~~~~~~~~~~~~~~~~
-float temperature_threshold = 22.0;
-int   water_level_threshold = 80;
-enum all_states {IDlE, DISABLED, ERROR, RUNNING};
+float temperature_threshold = 20.0;
+int   water_level_threshold = 400;
+int water_level;
+
+enum all_states {IDLE, DISABLED, ERROR, RUNNING};
+volatile all_states state = DISABLED;
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ~~~~~~~~~~~~~~~~~~~ Real Time Clock ~~~~~~~~~~~~~~~~~~~~~
+DS3231 clock;
+RTCDateTime dt;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~ Button  ~~~~~~~~~~~~~~~~~~~~~
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;
+
+int button_pin = 5; // 3
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// ~~~~~~~~~~~~~~~~~~~ Pot Button  ~~~~~~~~~~~~~~~~~~~~~
+unsigned long lastDebounceTime = 0; 
+unsigned long debounceDelay = 50;
+
+int pot_lastButtonState;
+int pot_buttonState;
+
+int pot_button_pin = 7; // 42
+int counter = 0;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 
 
@@ -88,8 +115,8 @@ void setup() {
 
   // ~~ Port E ~~
   //    Inputs
-  *ddr_e |= 0x0 << 5; // Temperature
-  *ddr_e |= 0x0 << 4; // Water
+  //*ddr_e |= 0x0 << 4; // Temperature
+  *port_e |= 0x1 << button_pin; // Button Pin w/ Pull Up
   //    Outputs
   *ddr_e |= 0x1 << 3; // Servo
 
@@ -104,34 +131,39 @@ void setup() {
   *ddr_L |= 0x01 << Red;
   *ddr_L |= 0x01 << Green;
   *ddr_L |= 0x01 << Blue;
+  *ddr_L |= 0x01 << Yellow;
+  //    Inputs
+  *port_L |= 0x1 << pot_button_pin; // Button Pin w/ Pull Up
+
 
   // setup the LCD
   lcd.begin(16, 2);
+
+  // setup Servo
+  myservo.attach(5);
 
   // setup the ADC
   adc_init();
 
   // setup the UART
   Serial.begin(9600);
+
+  // setup Clock
+  clock.begin();
+  clock.setDateTime(__DATE__, __TIME__);
 }
 
 
 void loop() {
 
   float temperature;
-  unsigned int water_level;
+  float humidity;
 
-  static all_states state;
-
-  if (/*button interupt flag ON*/ ) {
-    state = IDLE;
-  } else {
-    state = DISABLED;
-  }
 
   switch (state) {
     case IDLE:
-      print_temp_humid();
+
+      //Serial.println("Idle");
       LED_color('g');
 
 
@@ -143,18 +175,24 @@ void loop() {
 
       getTemperature( &temperature, &humidity);
       water_level = adc_read(water_level_pin);
+      getButtonPress();
 
-      // ~~ Exit Condition ~~
+      print_temp_humid(temperature, humidity);
+
+      //      ~~ Exit Condition ~~
       if ( temperature > temperature_threshold) {
         state = RUNNING;
-        break;
       } else if ( water_level < water_level_threshold) {
         state = ERROR;
-        break;
+      } else if (buttonState == 0) {
+        state = DISABLED;
       }
+
+
       break;
 
     case DISABLED:
+      // Serial.println("Disabled");
       lcd.clear();
       LED_color('y');
 
@@ -164,16 +202,15 @@ void loop() {
       // Motor OFF
       dc_fan(0);
 
-      // ~~ Exit Condition ~~
-      if ( /*button = !Kill*/) {
+      getButtonPress();
+      if (buttonState == 0) {
         state = IDLE;
-        break;
       }
 
       break;
     case RUNNING:
-      print_temp_humid();
-      LED_color('y');
+      //Serial.println("Running");
+      LED_color('b');
 
       // Servo Active
       pot_to_servo(1);
@@ -183,22 +220,30 @@ void loop() {
 
       getTemperature( &temperature, &humidity);
       water_level = adc_read(water_level_pin);
+      getButtonPress();
 
+      print_temp_humid(temperature, humidity);
+
+      //Serial.println( water_level);
       // ~~ Exit Condition ~~
-      if ( temperature < temperature_threshold) {
+      if ( temperature <= temperature_threshold) {
         state = IDLE;
-        break;
+
       } else if ( water_level < water_level_threshold) {
         state = ERROR;
-        break;
+
+      } else if (buttonState == 0) {
+        state = DISABLED;
       }
 
       break;
     case ERROR:
+      //Serial.println("Error");
+      lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print(" ERROR ");
       lcd.setCursor(0, 1);
-      lcd.print("Please Check Water Level ");
+      lcd.print("Water Empty :(   ");
       LED_color('r');
 
       // Servo Active
@@ -210,84 +255,117 @@ void loop() {
 
       getTemperature( &temperature, &humidity);
       water_level = adc_read(water_level_pin);
+      getButtonPress();
 
       // ~~ Exit Condition ~~
       if ( water_level > water_level_threshold) {
         state = IDLE;
-        break;
+      } else if (buttonState == 0) {
+        state = DISABLED;
       }
-      break;
-    default:
-      lcd.setCursor(0, 0);
-      lcd.print(" ERROR ");
-      lcd.setCursor(0, 1);
-      lcd.print("Please Check Water Level ");
-      LED_color('r');
 
-      // Servo Active
-      pot_to_servo(1);
-
-      // Motor OFF
-      dc_fan(0);
-
-
-      getTemperature( &temperature, &humidity);
-      water_level = adc_read(water_level_pin);
-
-      // ~~ Exit Condition ~~
-      if ( water_level > water_level_threshold) {
-        state = IDLE;
-        break;
-      }
       break;
   }
+}
 
+
+// Get Debounced Button Press
+void getButtonPress() {
+
+  if (*pin_e & (1 << button_pin)) {
+    buttonState = 1;
+  } else {
+    buttonState = 0;
+  }
+
+  if (buttonState != lastButtonState) {
+
+    if (buttonState == HIGH) {
+      buttonPushCounter++;
+    }
+    delay(75);
+  }
+  lastButtonState = buttonState;
 
 }
 
 //  ~~ Display Temp/Humidity ~~
-void print_temp_humid() {
-  float temperature;
-  float humidity;
+void print_temp_humid(float temperature, float humidity) {
 
-  getTemperature( &temperature, &humidity);
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(temperature);
   lcd.setCursor(0, 1);
   lcd.print("Humidity: ");
   lcd.print(humidity);
+
 }
+
+bool lastState;
 
 // ~~ DC Fan Code ~~
 void dc_fan(bool state) {
 
-
   if ( state  == false ) {
-    //Fan off
+    Serial.print("Motor Off: ");
     // 5 and 4 and 3 off
     *port_h &= 0x00;
+
+  lastState = false;
+    
   } else {
-    //Fan on
+     Serial.print("Motor On: ");
     //5 and 4 on 3 off
     *port_h |= 0x30;
+      lastState = true;
   }
+
+    dt = clock.getDateTime();
+    Serial.print(dt.year);   Serial.print("-");
+    Serial.print(dt.month);  Serial.print("-");
+    Serial.print(dt.day);    Serial.print(" ");
+    Serial.print(dt.hour);   Serial.print(":");
+    Serial.print(dt.minute); Serial.print(":");
+    Serial.print(dt.second); Serial.println("");
+    
 }
 
 // ~~ Potentiometer to Servo Enable/Disable ~~
-void pot_to_servo(bool state) {
-
+void pot_to_servo(int state) {
   if (state == 1) {
-    unsigned int val = adc_read(potpin);
-    val = map(val, 0, 1023, 0, 180);
-    myservo.write(val);
+    int reading;
+
+    if (*pin_L & (1 << pot_button_pin)) {
+      reading = 1;
+    } else {
+      reading = 0;
+    }
+
+
+    if (reading == 0) {
+
+      if (counter < 180) {
+        counter = counter + 60;
+      } else if (counter = 180) {
+        counter = 0;
+      }
+    }
+
+
+
+    myservo.write(counter);
+
     delay(15);
   }
 
 }
 
+
+
+
 // ~~ DH11 Temperature Sensor ~~
-static bool getTemperature( float *temperature, float *humidity ) {
+static bool getTemperature( float * temperature, float * humidity ) {
 
   if ( dht_sensor.measure( temperature, humidity ) == true )
   {
@@ -308,6 +386,8 @@ void LED_color(char color) {
   write_pL(Red, 0);
   write_pL(Green, 0);
   write_pL(Blue, 0);
+  write_pL(Yellow, 0);
+
 
   switch (color) {
 
@@ -321,12 +401,11 @@ void LED_color(char color) {
       write_pL(Blue, 1);
       break;
     case 'y':
-      write_pL(Red, 1);
-      *TCCR_5C |= (1 << COM_5_B1) | (1 << WGM_5_1);
-      *OCR_5B = 100;
+      write_pL(Yellow, 1);
       break;
-  }
 
+  }
+  delay(75);
 }
 void write_pL(unsigned char pin_num, unsigned char state) {
   if (state == 0) {
